6f8ac8e80ba9336f53210847524abbb7ece70696
IlTrovaClienti <magnetikaweb@gmail.com>
2025-05-14 23:40:25 +0200

    Add files via upload

---
+++ firebase-init.js
@@ -0,0 +1,13 @@
+// Paste your Firebase config below
+const firebaseConfig = {
+  apiKey: "AIzaSyAbiSZaWGRWTxA5C15TYv3IiuCTyS6WaOA",
+  authDomain: "iltrovaclienti-948af.firebaseapp.com",
+  projectId: "iltrovaclienti-948af",
+  storageBucket: "iltrovaclienti-948af.firebaseapp.com",
+  messagingSenderId: "774644367448",
+  appId: "1:774644367448:web:87bed07de4f922f959ca2d",
+  measurementId: "G-JF12YKNJX6"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
