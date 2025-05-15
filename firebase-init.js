c58c9b519dedecdfd5354420b82c70923d0e3072
IlTrovaClienti <magnetikaweb@gmail.com>
2025-05-16 01:03:59 +0200

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,11 +1,14 @@
-// sostituisci con le tue chiavi Firebase REALI solo in locale
+// firebase-init.js
+
 const firebaseConfig = {
   apiKey: "INSERISCI_API_KEY",
-  authDomain: "TUO_PROGETTO.firebaseapp.com",
-  projectId: "TUO_PROGETTO",
-  storageBucket: "TUO_PROGETTO.appspot.com",
-  messagingSenderId: "INSERISCI_ID",
+  authDomain: "INSERISCI_AUTH_DOMAIN",
+  projectId: "INSERISCI_PROJECT_ID",
+  storageBucket: "INSERISCI_BUCKET",
+  messagingSenderId: "INSERISCI_MESSAGING_ID",
   appId: "INSERISCI_APP_ID"
 };
 
 firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
