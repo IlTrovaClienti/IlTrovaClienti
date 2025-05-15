20bb2cafccd585be4eadb14bdcc0a5587e57b462
IlTrovaClienti <magnetikaweb@gmail.com>
2025-05-15 23:24:09 +0200

    Add files via upload

--- firebase-init.js
+++ firebase-init.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js";
-const firebaseConfig = {/* your config */};
+const firebaseConfig = { /* your config */ };
 const app = initializeApp(firebaseConfig);
 window.firebaseAuth = getAuth(app);
 window.firebaseDB = getFirestore(app);
