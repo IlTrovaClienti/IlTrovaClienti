8bf4ebbb2ec287a0aec3b793a6bd292b03002cbb
IlTrovaClienti <magnetikaweb@gmail.com>
2025-05-17 18:49:56 +0200

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,3 +1,17 @@
+// firebase-init.js
 
-// Inserisci qui la tua configurazione Firebase SOLO se ti serve.
-// Con i CDN compat l'app non va in errore anche se lasci vuoto.
+// ======== CONFIGURAZIONE FIREBASE (v9 compat) ========
+const firebaseConfig = {
+  apiKey:            "AIzaSyAb1SzaWGRWTxa5C1STYv3IiuCTyS6WaOA",
+  authDomain:        "iltrovaclienti-948af.firebaseapp.com",
+  projectId:         "iltrovaclienti-948af",
+  storageBucket:     "iltrovaclienti-948af.appspot.com",
+  messagingSenderId: "774644367448",
+  appId:             "1:774644367448:web:87bed07de4f922f959ca2d",
+  measurementId:     "G-JF12YKNJX6"
+};
+
+// ======== INIZIALIZZA FIREBASE ========
+firebase.initializeApp(firebaseConfig);
+const auth      = firebase.auth();
+const firestore = firebase.firestore();
