2cd8c798717334a8399cf99a13ab5106f35dbd50
IlTrovaClienti <magnetikaweb@gmail.com>
2025-05-19 17:52:38 +0200

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,12 +1,12 @@
-// Firebase v8 inizializzazione
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAbiSZaWGRWTxA5C15TYv3IiuCTyS6WaOA",
   authDomain: "iltrovaclienti-948af.firebaseapp.com",
-  projectId: "iltrovaclienti-948af",
-  storageBucket: "iltrovaclienti-948af.appspot.com",
+  projectId: "iltrovoclienti-948af",
+  storageBucket: "iltrovoclienti-948af.appspot.com",
   messagingSenderId: "774644367448",
   appId: "1:774644367448:web:87bed07de4f922f959ca2d",
   measurementId: "G-JF12YKNJX6"
 };
 firebase.initializeApp(firebaseConfig);
-// **Non dichiarare qui `const auth`**  
+const auth = firebase.auth();
