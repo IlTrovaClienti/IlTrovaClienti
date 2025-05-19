34b5477a733cae7af118dde08e44720289963be7
IlTrovaClienti <magnetikaweb@gmail.com>
2025-05-19 14:55:23 +0200

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,6 +1,8 @@
 // firebase-init.js
+
+// La tua configurazione Firebase
 const firebaseConfig = {
-  apiKey:   "AIzaSyAbiSZaWGRWTxA5C15TYv3IiuCTyS6WaOA",
+  apiKey: "AIzaSyAbiSZaWGRWTxA5C15TYv3IiuCTyS6WaOA",
   authDomain: "iltrovaclienti-948af.firebaseapp.com",
   projectId: "iltrovaclienti-948af",
   storageBucket: "iltrovaclienti-948af.appspot.com",
@@ -8,5 +10,9 @@ const firebaseConfig = {
   appId: "1:774644367448:web:87bed07de4f922f959ca2d",
   measurementId: "G-JF12YKNJX6"
 };
+
+// Inizializzo Firebase
 firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
+
+// Esporto l’istanza di Auth su window, così non devo riscrivere `const auth` altrove
+window.auth = firebase.auth();
